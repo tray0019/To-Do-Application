7323f2131c47678243d0ee57c992d3a6baf72aeb
tray0019 <tray0019@algonquinlive.com>
2025-08-08 10:12:03 -0400

    I have set a @Service annotation in my TaskService class and I created my getAllTask method, this method return all task thanks to the TaskRepository jpa-hybernate methods that automatically query the result in the database.

--- src/main/java/org/trayvilla/todoapp/services/TaskService.java
+++ src/main/java/org/trayvilla/todoapp/services/TaskService.java
@@ -1,9 +1,21 @@
 package org.trayvilla.todoapp.services;
 
 import org.springframework.stereotype.Service;
+import org.trayvilla.todoapp.models.Task;
+import org.trayvilla.todoapp.repository.TaskRepository;
+
+import java.util.List;
 
 @Service
 public class TaskService {
 
+    private final TaskRepository taskRepository;
 
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
 }
